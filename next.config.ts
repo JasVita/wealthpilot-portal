963042b552fae1fa2b42fd7cec1effc2a213c4bf
turoid <jasmine@vitalogy.ai>
2025-08-28 03:44:26 +0000

    tmp push

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // ⛔️ Don’t run ESLint during `next build`
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
